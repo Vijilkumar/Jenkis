9614a13160db28dc5973e612107d8376a41c6275
root <root@ip-10-0-4-110.us-east-2.compute.internal>
2018-03-06 02:28:52 +0000

    from server

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! this is from jenkins server" );
         System.exit( 0 ); //success
     }
 }
